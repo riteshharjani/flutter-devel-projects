c22925f8ff4f1d1a1453972ff0bad5e96d20c475
Ritesh Harjani <ritesh.harjani@gmail.com>
2019-12-04 22:22:28 +0530

    Add Named Routes
    Signed-off-by: Ritesh Harjani <ritesh.harjani@gmail.com>

--- navigation_screens/lib/main.dart
+++ navigation_screens/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 //Source:
 //https://flutter.dev/docs/cookbook/navigation/navigation-basics
 
-void main()
+void main1()
 {
   runApp(
     MaterialApp(
@@ -53,6 +53,63 @@ class SecondRoute extends StatelessWidget {
         child: RaisedButton(
           onPressed: () => Navigator.pop(context),
           child: Text("Go Back"),
+        ),
+      ),
+    );
+  }
+}
+
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstNamedRoute(),
+        '/SecondPage': (context) => SecondNamedRoute(),
+      },
+    ),
+  );
+}
+
+class FirstNamedRoute extends StatelessWidget {
+  const FirstNamedRoute({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => print("menu pressed"),
+          icon: Icon(Icons.menu),
+        ),
+        title: Text("First Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Open route1"),
+          onPressed: () => Navigator.pushNamed(context, '/SecondPage'),
+        ),
+      ),
+    );
+
+  }
+}
+
+class SecondNamedRoute extends StatelessWidget {
+  const SecondNamedRoute({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text("Go Back2"),
         ),
       ),
     );
