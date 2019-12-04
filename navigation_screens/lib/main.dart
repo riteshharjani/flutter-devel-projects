a6e1488054f36f89599653eadea4dcb327331b41
Ritesh Harjani <ritesh.harjani@gmail.com>
2019-12-04 22:06:06 +0530

    Default Scaffold also provides back navigation
    Signed-off-by: Ritesh Harjani <ritesh.harjani@gmail.com>

--- navigation_screens/lib/main.dart
+++ navigation_screens/lib/main.dart
@@ -47,10 +47,6 @@ class SecondRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
         title: Text("Second page"),
       ),
       body: Center(
